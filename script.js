395c8c6f8288908a0fc1d0b7eedd2cdf20224402
ngzhekai <ngzhekai@gmail.com>
2021-12-05 22:54:27 +0800

    fix typo in index.html and script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ for (var i = 0; i < names.length; i++) {
 
   var firstLetter = names[i].charAt(0);
 
-  if (firstLetter.toLowerCase() == 'j') {
+  if (firstLetter.toLowerCase() === 'j') {
     byeSpeaker.speak(names[i]);
   }
   else {
